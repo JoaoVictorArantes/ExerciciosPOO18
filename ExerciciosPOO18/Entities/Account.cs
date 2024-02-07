c9f2c8e9ce0de1e5a93d6e3920bc65f53c5f30bf
João Victor <jvaarantes@gmail.com>
2024-02-07 11:55:28 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosPOO18/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using ExerciciosPOO18.Entities.Exceptions;
+
+namespace ExerciciosPOO18.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public String Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void deposit(double Amount)
+        {
+            Balance += Amount;
+        }
+        public void Withdraw(double Amount)
+        {
+            if (Balance < Amount)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (Amount > WithdrawLimit)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            Balance -= Amount;
+        }
+    }
+}
---
+++ ExerciciosPOO18/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExerciciosPOO18.Entities;
+
+namespace ExerciciosPOO18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            String Name = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double InitialBalance = double.Parse(Console.ReadLine());
+
+            Console.Write("Withdraw limit: ");
+            double WithdrawLimit = double.Parse(Console.ReadLine());
+
+            Account Account = new Account(number, Name, InitialBalance, WithdrawLimit);
+
+            Console.WriteLine("\nEnter amount for withdraw: ");
+            double Withdraw = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Account.Withdraw(Withdraw);
+                Console.WriteLine("New balance: " + Account.Balance.ToString("F2"));
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Withdraw error: " + ex.Message);
+            }
+
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
